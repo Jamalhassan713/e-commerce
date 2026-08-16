8f88efbb6a6b32a172b5787db910b90e4189d1cf
Jamal hassan <jamalhassan457@gmail.com>
2026-08-16 14:08:22 +0300

    add more data

--- src/common/services/email.service.ts
+++ src/common/services/email.service.ts
@@ -12,8 +12,9 @@ export class EmailService {
 
         const transporter = nodemailer.createTransport({
             host: "smtp.gmail.com",
-            port: 465,
-            secure: true,
+            port: 587,
+            secure: false,
+            requireTLS: true,
             family: 4,
             auth: {
                 user: process.env.USER_EMAIL,
